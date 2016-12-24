a335be2241431e08c10b6f14fae7a3aa7dceeb9c
vantrong0202 <vantrong.hus@gmail.com>
2016-12-24 09:25:36 +0700

    add share

--- resources/views/frontend/detail-technology.blade.php
+++ resources/views/frontend/detail-technology.blade.php
@@ -221,8 +221,6 @@
 			</div>
 		</div>
 	</div>
-	<!-- Go to www.addthis.com/dashboard to customize your tools -->
-	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54d043744f8bfa02"></script>
 @endsection
 
 @section('after-scripts')
